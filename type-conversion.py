8f6bee258d98bfe26b5489dc3cac83163f9248a4
Abhilasha03 <abhilashawaghmare03@gmail.com>
2025-05-06 21:06:34 +0530

    type-conversion.py

---
+++ type-conversion.py
@@ -0,0 +1,7 @@
+old_age = input("enter your old age : ")
+int(old_age) #integer represents integers means numbers
+new_age = int(old_age) + 2
+print(new_age)
+
+number = 18
+print(float(18)) #float type
